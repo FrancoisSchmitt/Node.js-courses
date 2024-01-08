ab18eb64ccd0bf33c812d7ad5b2a5bf00f39658b
Schmitt François <Schmitt.francois.1699@gmail.com>
2024-01-08 18:57:50 +0100

    adding sequilise

--- models/product.js
+++ models/product.js
@@ -1,30 +1,27 @@
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequileze = require('sequelize');
 
+const sequelize = require('../util/database');
 
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequileze.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: Sequileze.STRING,
+    price: {
+        type: Sequileze.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequileze.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: Sequileze.STRING,
+        allowNull: false,
     }
 
-    save() {
-        return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?,?,?,?)',
-            [this.title, this.price, this.imageUrl, this.description]);
-    }
-
-    static deleteById(id) {
-    }
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products')
-    }
-
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-
-    }
-};
+});
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequileze = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complet',
-    password: 'Skews2728'
+const sequileze = new Sequileze('node-complet', 'root', 'Skews2728', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequileze;
