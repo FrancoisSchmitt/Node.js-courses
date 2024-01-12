d952fa8a7b94cc116b3680e1f6a0dc415b6c58e1
Schmitt François <Schmitt.francois.1699@gmail.com>
2024-01-12 14:07:36 +0100

    adding signup/ login & LF CSRF

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
-const User = require('../models/user')
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
 	// const isLoggedIn = req.get("Cookie").trim().split("=")[1] === "true";
 	console.log(req.session);
@@ -9,21 +12,71 @@ exports.getLogin = (req, res, next) => {
 	});
 };
 
+exports.getSignup = (req, res, next) => {
+	res.render("auth/signup", {
+		path: "/signup",
+		pageTitle: "Signup",
+		isAuthenticated: false,
+	});
+};
 exports.postLogin = (req, res, next) => {
-	User.findById("659ff5eccfa4279f6cf5290e")
+	const email = req.body.email;
+	const password = req.body.password;
+	User.findOne({ email: email })
 		.then((user) => {
-			req.session.user = user;
-			req.session.isLoggedIn = true
-			req.session.save((err) => {
-				console.log(err);
-				res.redirect('/')
-			});
+			if (!user) {
+				return res.redirect("/login");
+			}
+			bcrypt
+				.compare(password, user.password)
+				.then((doMatch) => {
+					if (doMatch) {
+						req.session.user = user;
+						req.session.isLoggedIn = true;
+						return req.session.save((err) => {
+							console.log(err);
+							res.redirect("/");
+						});
+					}
+					res.redirect("/login");
+				})
+				.catch((err) => {
+					console.log(err);
+					res.redirect("/login");
+				});
 		})
 		.catch((err) => console.log(err));
 };
+exports.postSignup = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const confirmPassword = req.body.confirmPassword;
 
+	User.findOne({ email: email })
+		.then((userDoc) => {
+			if (userDoc) {
+				return res.redirct("/signup");
+			}
+			return bcrypt
+				.hash(password, 12)
+				.then((hashedPassword) => {
+					const user = new User({
+						email: email,
+						password: hashedPassword,
+						cart: { items: [] },
+					});
+					return user.save();
+				})
+				.then((result) => {
+					res.redirect("/login");
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
 exports.postLogout = (req, res, next) => {
 	req.session.destroy(() => {
-		res.redirect('/');
-})
+		res.redirect("/");
+	});
 };
--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const shopController = require('../controllers/shop')
 const router = express.Router();
 
 const adminData = require('./admin');
-
+const isAuth = require('../middleware/is-auth')
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
@@ -16,14 +16,14 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+router.get("/cart", isAuth, shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+router.post("/cart", isAuth, shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCardDeleteProduct);
+router.post("/cart-delete-item", isAuth, shopController.postCardDeleteProduct);
 
-router.post('/create-order', shopController.postOrder)
+router.post("/create-order", isAuth, shopController.postOrder);
 
-router.get('/orders', shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
 module.exports = router;
